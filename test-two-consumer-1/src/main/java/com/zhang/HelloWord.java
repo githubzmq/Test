ad81ce26f5c250d5055753be75e310f9d4e1e339
zhangmingqing <757023167@qq.com>
2016-05-18 16:51:28 +0800

    2016.5.18新建一个项目

---
+++ test-two-consumer-1/src/main/java/com/zhang/HelloWord.java
@@ -0,0 +1,22 @@
+package com.zhang;
+
+import java.util.Date;
+
+/**
+ * @ClassName HelloWord
+ * @Description 第一个项目
+ * @author zhangmingqing
+ * @date 2016/4/11 15:27
+ */
+public class HelloWord {
+    /**
+     * @MethodName
+     * @Description 打印时间
+     * @author zhangmingqing
+     * @date 2016/4/11 15:32
+     */
+    public static void main(String[] args){
+        Date date=new Date();
+        System.out.println("HelloWord.main"+date);
+    }
+}
