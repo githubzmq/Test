9806ad0e9cde6217de6f4dd4d8aa007747b887e5
zhangmingqing <757023167@qq.com>
2016-05-18 17:36:46 +0800

    增加了个方法

--- test-two-consumer-1/src/main/java/com/zhang/HelloWord.java
+++ test-two-consumer-1/src/main/java/com/zhang/HelloWord.java
@@ -16,7 +16,22 @@ public class HelloWord {
      * @date 2016/4/11 15:32
      */
     public static void main(String[] args){
+        String str = "你知道我在等你吗？";
         Date date=new Date();
+        HelloWord helloWord = new HelloWord();
+        helloWord.gitTest(str);
         System.out.println("HelloWord.main"+date);
+    }
+    /**
+     * @MethodName gitTest
+     * @Description
+     * @param s 参数
+     * @return void
+     * @throws
+     * @author zhangmingqing
+     * @date 2016/5/18 17:34
+     */
+    public void gitTest(String s){
+        System.out.println("这个是动态获取的值=====>"+s);
     }
 }
